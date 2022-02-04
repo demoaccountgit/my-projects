f39181b6742ea12f6b7921d2ce438473ec739376
shadab <mohammed.shadabansari@192.168.1.4>
2022-02-05 00:55:38 +0530

    EC2_AMI_20_21

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
+variable instance_type {}
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -43,8 +44,7 @@ resource "aws_default_route_table" "main-rtb" {
     }
 }
 
-resource "aws_security_group" "myapp-sg" {
-    name = "myapp-sg"
+resource "aws_default_security_group" "default-sg" {
     vpc_id = aws_vpc.myapp-vpc.id
 
     ingress {
@@ -67,6 +67,40 @@ resource "aws_security_group" "myapp-sg" {
         prefix_list_ids = []
     }
     tags = {
-        Name: "${var.env_prefix}-sg"
+        Name: "${var.env_prefix}-default-sg"
     }
 }
+
+data "aws_ami" "latest-amazon-machine-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+        }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+    
+}
+
+output "aws_ami_id" {
+  value       = "data.aws_ami.latest-amazon-machine-image.id"
+}
+
+resource "aws_instance" "my-app-server" {
+    ami = data.aws_ami.latest-amazon-machine-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+    associate_public_ip_address = true
+    key_name = "My_project"
+
+    tags = {
+        Name = "${var.env_prefix}-server"
+    }
+}
+
