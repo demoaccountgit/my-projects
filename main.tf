58933fb9d67fbbaf094f47a476a99cb1b41bec8b
shadab <mohammed.shadabansari@192.168.1.4>
2022-02-04 23:16:34 +0530

    DefRT+SG_18_19

--- main.tf
+++ main.tf
@@ -1,28 +1,72 @@
-provider "aws" {}
-
-variable  "cidr_blocks" {
-  description = "subnet cidr block and name tags as of environemt"
-  type = list(object 
-  ({
-      cidr_block = string
-      name = string
-  }))
-
+provider "aws" {
+    region = "eu-west-3"
 }
 
+variable vpc_cidr_blocks {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_blocks
     tags = {
-        Name = var.cidr_blocks[0].name
+        Name = "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet1" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "ap-south-1a"
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags = {
-        Name = var.cidr_blocks[1].name
+        Name = "${var.env_prefix}-subnet-1"
+    }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name: "${var.env_prefix}-igw"
+    }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+        route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name: "${var.env_prefix}-main-rtb"
+    }
+}
+
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]  
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+    tags = {
+        Name: "${var.env_prefix}-sg"
     }
 }
