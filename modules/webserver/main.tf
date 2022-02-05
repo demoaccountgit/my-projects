16ab71b60c5d53ca72e60d8d679972e5f79e9b15
shadab <mohammed.shadabansari@192.168.1.4>
2022-02-05 14:47:57 +0530

    vpc_module_3.11.5

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -1,4 +1,5 @@
-resource "aws_default_security_group" "default-sg" {
+resource "aws_security_group" "myapp-sg" {
+    name = "myapp-sg"
     vpc_id = var.vpc_id
 
     ingress {
@@ -50,7 +51,7 @@ resource "aws_instance" "my-app-server" {
     instance_type = var.instance_type
 
     subnet_id = var.subnet_id
-    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
     availability_zone = var.avail_zone
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
